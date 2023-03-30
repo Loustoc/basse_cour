12fb45512f943cb44952d11e3c6713cf62910897
Lou K <84414167+Loustoc@users.noreply.github.com>
2023-03-30 15:44:44 +0000

    test even lower

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function start_anim() {
   setTimeout(function () {
     document.getElementById("anim_marteau").src =
       "img/anim/basse-cour-anim0074.svg";
-  },2900);
+  },2800);
     setTimeout(function(){
        fade_anim();
         },3800);
